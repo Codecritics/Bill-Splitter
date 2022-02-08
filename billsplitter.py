89d93ed8396b2ca880869525135439a4555406a4
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-02-08 11:33:38 +0100

    Stage 2/4

--- billsplitter.py
+++ billsplitter.py
@@ -1,10 +1,30 @@
+bill_value, guests, nb_people = 0, dict(), 0
+
+
+def print_divided_bill():
+    print()
+    print(guests)
+
+
+def split_bill():
+    global guests
+
+    people_part = round(bill_value / nb_people, 2)
+    for guest in guests:
+        guests[guest] = people_part
+
+
 def define_dinner_party():
+    global bill_value, guests, nb_people
     print("Enter the number of friends joining (including you):")
     nb_people = int(input())
     print()
-    guests = dict()
-    if nb_people <= 0:
-        print("No one is joining for the party")
+    try:
+        message = "No one is joining for the party"
+        assert nb_people > 0, message
+    except AssertionError as err:
+        print(err)
+        nb_people = 0
     else:
         print("Enter the name of every friend (including you), each on a new line:")
         for _ in range(nb_people):
@@ -13,7 +33,11 @@ def define_dinner_party():
             if len(person) == 0:
                 break
         print()
-        print(guests)
+        print("Enter the total bill value:")
+        bill_value = int(input())
 
 
 define_dinner_party()
+if nb_people > 0:
+    split_bill()
+    print_divided_bill()
